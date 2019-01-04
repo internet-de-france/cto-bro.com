1ace71ded5ad4c48e9e254265cc8480e586a506f
Alex Hoyau <a.hoyau@silexlabs.org>
2019-01-04 17:00:43 +0100

    small design changes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
 
-/*
-$(function() {
-    var siteWidth = $('meta[name=website-width]').attr('content');
-    function doResize() {
-        var viewportWidth = $(window).width();
-        if(viewportWidth > 480 && viewportWidth < siteWidth) {
-            $('body').css({
-              'transform': 'scale(' + (viewportWidth / 1200) + ')',
-              'transform-origin': '0 0',
-            })
-        }
-        else {
-            $('body').css({
-              'transform': '',
-              'transform-origin': '',
-            })
-        }
-    }
-    if(siteWidth) {
-        $(window).resize(doResize);
-        doResize();
-    }
-});
-*/
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
