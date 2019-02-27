a5a9ba87f4ca8e1002eaadff229d346ae9b35bb6
Alex Hoyau <a.hoyau@silexlabs.org>
2019-02-27 16:59:30 -0600

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 
+function testScroll() {
+    var scroll = $('html').scrollTop();
+    if(scroll > 200) {
+        document.body.classList.add('scrolled');
+        document.body.classList.remove('not-scrolled');
+    }
+    else {
+        document.body.classList.remove('scrolled');
+        document.body.classList.add('not-scrolled');
+    }
+}
+$(function() {
+    window.addEventListener('scroll', testScroll);
+    testScroll();
+})
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
